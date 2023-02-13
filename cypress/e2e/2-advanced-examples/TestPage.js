76a232c9af59d997199eb529c44191beadcb40d4
Nishigandha Patil <58659856+nishigandhap@users.noreply.github.com>
2023-02-13 10:30:35 +0100

    Update TestPage.js
    Updated test case name.

--- cypress/e2e/2-advanced-examples/TestPage.js
+++ cypress/e2e/2-advanced-examples/TestPage.js
@@ -13,11 +13,11 @@ describe("Test suite", function () {
         HomePageModel.verifyBookmarks()
 
     })
-    it("Enter incorrect bookmark and verify error message is displayed", function () {
+    it("Enter incorrect bookmark name and verify error message is displayed", function () {
         HomePageModel.enterIncorrectTextInSearchField()
 
     })
-    it("Fill the details in fields and do click on send message button", function () {
+    it("Fill the details in fields and do not click on send message button", function () {
         HomePageModel.clickCompanyButton()
         HomePageModel.enterDetailsInForm('Laxmi', 'Namah', 'laxmi@mostlyai.com', 978067665, 'AI', 'Thank you')
         HomePageModel.hoverOnSendMessageButton()
